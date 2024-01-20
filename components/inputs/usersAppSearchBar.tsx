a71f2920924897875cdb869192ef454ada70a2e2
oliJapa <olialexander1131@gmail.com>
2024-01-20 15:38:30 -0400

    Feat Users App view done

---
+++ components/inputs/usersAppSearchBar.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Magnifyinglass from '@/public/Magnifyinglass.svg'
+import ExchangeNestLogo from '@/public/exchangeNestLogoPlain.svg'
+
+const UsersAppSearchBar = () => {
+    return (
+        <div className="h-[60px] w-full border border-[#2196F3] rounded-[30px] flex items-center justify-between px-5">
+            <Magnifyinglass className={' w-4 h-4 '} />
+            <input type="text" className='w-full px-3' placeholder='Where to go ?' />
+            <ExchangeNestLogo className='w-[57px] ml-[-10px] stroke-[3px]' />
+        </div>
+    )
+}
+
+export default UsersAppSearchBar
